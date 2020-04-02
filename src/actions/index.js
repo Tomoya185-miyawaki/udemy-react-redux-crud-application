4cd255148284fc1b16e246a038cd57a3e04260d7
Tomoya <tomo16185@gmail.com>
2020-04-02 20:59:07 +0900

    implement reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT'
+export const DECREMENT = 'DECREMENT';
 
 export const increment = () => ({
-    type: INCREMENT
+  type: INCREMENT
 })
 export const decrement = () => ({
   type: DECREMENT
